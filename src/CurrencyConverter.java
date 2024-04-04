ca071735bdb24f8292a6c1fa19c41e9bd0f53c46
Kristin Obermyer <ms-O@users.noreply.github.com>
2024-04-04 11:09:00 -0400

    Add example message

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -3,6 +3,7 @@ public class CurrencyConverter {
 
     public static void main(String[] args) {
         System.out.println("Hello Currency Converter");
+        System.out.println("Added code on dev branch");
     }
 
 }
